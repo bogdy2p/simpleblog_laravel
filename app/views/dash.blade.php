bfc326599dc6375f3eae49fe69dee53a0caa4561
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-24 13:56:41 +0200

    More views implementation

--- app/views/dash.blade.php
+++ app/views/dash.blade.php
@@ -1,8 +1,33 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="small-3 large-3 column">
+    <aside class="sidebar">
+        <h3>Menu</h3>
+        <ul>
+            <li>{{HTML::linl('/','Home')}}</li>
+            <li class="divider"></li>
+            <li class="{{ (strpos(URL::current(),route('post.new'))!== false)? 'active' : ''}}">
+                {{HTML::linkRoute('post.new','New Post')}}
+            </li>
+            <li class="{{ (strpos(URL::current(),route('post.list'))!== false)? 'active': ''}}">
+                {{HTML::linkRoute('post.list','List Posts')}}
+            </li>
+            <li class="divider"></li>
+            <li class="{{ (strpos(URL::current(),route('comment.list'))!== false)? 'active': ''}}">
+                {{HTML::linkRoute('comment.list','List Comments')}}
+            </li>
+        </ul>
+    </aside>
+</div>
+<div class="small-9 large-9 column">
+    <div class="content">
+        @if(Session::has('success'))
+        <div data-alert class="alert-box round">
+            {{Session::get('success')}}
+            <a href="#" class="close">&times;</a>
+        </div>
+        @endif
+        {{$content}}
+    </div>
+    <div class="reveal-modal small" id="comment-show" data-reveal>
+        {{-- quick comment using ajax --}}
+    </div>
+</div>
--- app/views/posts/list.blade.php
+++ app/views/posts/list.blade.php
@@ -1,8 +1,18 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h2 class="post-listings">Post Listings</h2><hr>
+<table>
+    <thead>
+        <tr>
+            <th width="300">Post Title</th>
+            <th width="120">Post Edit</th>
+            <th width="120">Post Delete</th>
+            <th width="120">Post View</th>
+        
+        
+        
+        
+    </thead>
+    
+    
+    
+    
+</table>
