b434de88423350d8e4b6d1659e957e5ce66105e0
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-24 10:25:10 +0200

    Renamed postcontroller and created CommentsController sketch

---
+++ app/controllers/CommentsComtroller.php
@@ -0,0 +1,28 @@
+<?php
+
+class CommentController extends BaseController {
+    /* functii get */
+
+    public function listComment() {
+        
+    }
+
+    public function newComment() {
+        
+    }
+
+    public function showComment(Comment $comment) {
+        
+    }
+
+    public function deleteComment(Comment $comment) {
+        
+    }
+
+    /* functii post */
+
+    public function updateComment(Comment $comment) {
+        
+    }
+
+}
