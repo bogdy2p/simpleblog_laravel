ec3a127cfca04f9e3b59d5e15e0d4252016f57b7
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-24 10:54:12 +0200

    Implemented Comments Controller

--- app/controllers/CommentsComtroller.php
+++ app/controllers/CommentsComtroller.php
@@ -4,25 +4,48 @@ class CommentController extends BaseController {
     /* functii get */
 
     public function listComment() {
-        
+        $comments = Comment::orderBy('id', 'desc')->paginate(20);
+        $this->layout->title = 'Comment Listings';
+        $this->layout->main = View::make('dash')->nest('content', 'comments.list', compact('comments'));
     }
 
     public function newComment() {
-        
+        $comment = [];
+        $rules = [];
+        $valid = Validator::make($comment, $rules);
+        if ($valid->passes()) {
+            $comment = new Comment($comment);
+            $comment->approved = 'no';
+            $post->comments()->save($comment);
+            /* redirect to the form portion in the page */
+            return Redirect::to(URL::previous() . '#reply')->with('success', 'Comment has been submitted and waiting for approval!');
+        } else {
+            return Redirect::to(URL::previous() . '#reply')->withErrors($valid)->withInput();
+        }
     }
 
     public function showComment(Comment $comment) {
-        
+        if (Request::ajax())
+            return View::make('comments.show', compact('comment'));
+        // non ajax calls in else.. ?
     }
 
     public function deleteComment(Comment $comment) {
-        
+        $post = $comment->post;
+        $status = $comment->approved;
+        $comment->delete();
+        ($status === 'yes') ? $post->decrement('comment_count') : '';
+        return Redirect::back()->with('success', 'Comment deleted!');
     }
 
     /* functii post */
 
     public function updateComment(Comment $comment) {
-        
+        $comment->approved = Input::get('status');
+        $comment->save();
+        $comment->post->comment_count = Comment::where('post_id', '=', $comment->post->id)->where('approved', '=', 1)->count();
+        $comment->post->save();
+        return Redirect::back()->with('success', 'Comment ' . (($comment->approved === 'yes') ? 'Approved' : 'Disapproved'));
     }
 
 }
