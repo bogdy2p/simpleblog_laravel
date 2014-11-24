e9f3cf2f875e3da4d7adb4eb824a00693fec5d60
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-24 15:05:35 +0200

    Fixed Comment and User model too , added fillable array

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -2,19 +2,17 @@
 
 class Comment extends Eloquent {
 
+    protected $fillable = ['post_id', 'email', 'commenter', 'comment', 'approved', 'created_at', 'updated_at'];
+
     /**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'comments';
-    
-        
-    
-    
-    
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
+
     public function post() {
-        
+
         return $this->belongsTo('Post');
     }
 
--- app/models/Post.php
+++ app/models/Post.php
@@ -2,24 +2,15 @@
 
 class Post extends Eloquent {
 
-    
-    
-        protected $fillable = ['title', 'read_more', 'content','comment_count','created_at','updated_at'];
-    
-    
-    
-    
+    protected $fillable = ['title', 'read_more', 'content', 'comment_count', 'created_at', 'updated_at'];
+
     /**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'posts';
-        
-        
-    
-    
-    
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+
     public function comments() {
 
         return $this->hasMany('Comment');
